eddb1256f90dc76886be3e84eb8ba9754331dc14
Victor Tounaregha <155453116+victwa2baba@users.noreply.github.com>
2025-02-02 16:48:50 +0100

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -1,99 +1,56 @@
-/**
- * Copyright 2016-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the license found in the  
- * LICENSE file in the root directory of this source tree.
- */
-
 var bodyParser = require('body-parser');
 var express = require('express');
+var request = require('request');
 var app = express();
-var xhub = require('express-x-hub');
+
+const CLIENT_ID = process.env.INSTAGRAM_CLIENT_ID;
+const CLIENT_SECRET = process.env.INSTAGRAM_CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
 
-app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
-var token = process.env.TOKEN || 'token';
-var received_updates = [];
-var posts = []; // Array to store posts
-
 app.get('/', function(req, res) {
-  console.log(req);
-  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+  res.send('Welcome to the Instagram Login API!');
 });
 
-// Instagram login route  
-app.get('/instagram/login', function(req, res) {
-  // Redirect to Instagram login page here  
-  res.redirect('https://api.instagram.com/oauth/authorize?client_id=' + process.env.INSTAGRAM_CLIENT_ID + '&redirect_uri=' + process.env.REDIRECT_URI + '&scope=user_profile,user_media&response_type=code');
+// Redirect to Instagram for authentication  
+app.get('/auth/instagram', function(req, res) {
+  const authUrl = `https://api.instagram.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=user_profile,user_media&response_type=code`;
+  res.redirect(authUrl);
 });
 
-// Handle Instagram callback  
-app.get('/instagram/callback', function(req, res) {
-  var code = req.query.code;
-  // Exchange the code for an access token  
-  // Implement token fetching here and store it  
-  res.send('Login successful. You can now create posts.');
-});
+// Handle redirect from Instagram  
+app.get('/auth/instagram/callback', function(req, res) {
+  const code = req.query.code;
+  
+  if (code) {
+    const options = {
+      url: 'https://api.instagram.com/oauth/access_token',
+      method: 'POST',
+      form: {
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        grant_type: 'authorization_code',
+        redirect_uri: REDIRECT_URI,
+        code: code  
+      }
+    };
 
-// Create a new post  
-app.post('/instagram/post', function(req, res) {
-  var newPost = req.body; // Assume the body contains post data  
-  posts.push(newPost); // Add post to the array  
-  console.log('New post created:', newPost);
-  res.sendStatus(201);
-});
-
-// View all posts  
-app.get('/instagram/posts', function(req, res) {
-  res.send(posts); // Return the posts array  
-});
-
-// Webhook routes  
-app.get(['/facebook', '/instagram', '/threads'], function(req, res) {
-  if (
-    req.query['hub.mode'] == 'subscribe' &&
-    req.query['hub.verify_token'] == token  
-  ) {
-    res.send(req.query['hub.challenge']);
+    request(options, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        const data = JSON.parse(body);
+        // Handle successful login here  
+        res.send(`<pre>${JSON.stringify(data, null, 2)}</pre>`);
+      } else {
+        res.sendStatus(400);
+      }
+    });
   } else {
     res.sendStatus(400);
   }
-});
-
-app.post('/facebook', function(req, res) {
-  console.log('Facebook request body:', req.body);
-
-  if (!req.isXHubValid()) {
-    console.log('Warning - request header X-Hub-Signature not present or invalid');
-    res.sendStatus(401);
-    return;
-  }
-
-  console.log('request header X-Hub-Signature validated');
-  // Process the Facebook updates here  
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
-
-app.post('/instagram', function(req, res) {
-  console.log('Instagram request body:');
-  console.log(req.body);
-  // Process the Instagram updates here  
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
-});
-
-app.post('/threads', function(req, res) {
-  console.log('Threads request body:');
-  console.log(req.body);
-  // Process the Threads updates here  
-  received_updates.unshift(req.body);
-  res.sendStatus(200);
 });
 
 app.listen();
